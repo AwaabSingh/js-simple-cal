b684b7e90357452bd1a069c263d089fa3b4981eb
AwaabSingh <adeoyin2828@gmail.com>
2022-06-21 22:46:02 +0100

    done

---
+++ cal.js
@@ -0,0 +1,17 @@
+alert('Welcome to javascript calculator')
+const num1 = parseInt(prompt('Enter frist number'))
+const num2 = parseInt(prompt('Enter second number'))
+const math = prompt('enter either +, -, *, /')
+
+if (math === '+') {
+    alert(num1 + num2)
+} else if (math === '-') { 
+    alert(num1 - num2)
+}else if (math === '*') { 
+    alert(num1 * num2)
+} else if (math === '/') { 
+    alert(num1 / num2)
+} 
+else {
+    alert('Invalid Symbol.. Try again')
+}
